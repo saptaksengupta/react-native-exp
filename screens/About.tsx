35f2b4e8b3a1a03a3eb3e30164c4d8f71eb8fe92
Saptak Sengupta <deeps.sengupta@gmail.com>
2020-08-14 07:33:32 +0530

    [master] Animation kickstart 🏁

--- screens/About.tsx
+++ screens/About.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View, StatusBar } from "react-native"; 
+import { StyleSheet, Text, View, Button } from "react-native"; 
 
 const styles = StyleSheet.create({
     container: {
     }
 });
 
-const About = () => {
+const About = ({navigation}) => {
     return (
         <View style={styles.container}>
             <Text>About Page Content!</Text>
+            <Button title="Go back" onPress={() => navigation.goBack()} />
         </View>
     )
 };
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,18 +1,44 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState, useRef } from "react";
+import { StyleSheet, Text, View, Button, Animated } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
-const Home = ({ navigation }) => {
+const Home = ({ navigation }: any) => {
+  const value = useRef(new Animated.ValueXY({x: 0, y: 0})).current;
+
+  const moveBall = () => {
+    alert(1);
+    Animated.timing(value, {
+      toValue: {x: 100, y: 100},
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Home Page Content!</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate("About")}
-      />
+      <View>
+        <Animated.View
+          style={{
+            width: 100,
+            height: 100,
+            transform: [{translateX: value.x}, {translateY: value.y}],
+            backgroundColor: "#444",
+            borderRadius: 100 / 2,
+          }}
+        >
+        </Animated.View>
+      </View>
+      <TouchableOpacity onPress={moveBall}>
+        <Text>Moave the ball!</Text>
+      </TouchableOpacity>
     </View>
   );
 };
